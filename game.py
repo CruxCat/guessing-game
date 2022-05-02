0977d5bc6929476ae6324a9b7ac4e12445a3d27d
Catherine Fox <adalovelace@Adas-MacBook-Pro.local>
2022-05-02 15:32:57 -0700

    Initial commit

---
+++ game.py
@@ -0,0 +1,14 @@
+"""A number-guessing game."""
+
+# Put your code here
+print("hi")
+#greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
